e960cd3cacada7c5151f34e0f1af53776d171864
Dominik Vach <vach.dominik@gmail.com>
2026-01-04 16:47:13 +0100

    release 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name='flogg',
-    version='1.0.0',
+    version='1.0.1',
     author='DovaX',
     author_email='dovax.ai@gmail.com',
     description='Improved logging system for Python',
